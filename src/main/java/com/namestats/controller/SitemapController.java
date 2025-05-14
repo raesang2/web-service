83ab6e5e654d9b169275c74b0fa2f39423d1baf0
raesang2 <raesang2@DESKTOP-HS65R85>
2025-05-14 22:42:49 +0900

    u

--- src/main/java/com/namestats/controller/SitemapController.java
+++ src/main/java/com/namestats/controller/SitemapController.java
@@ -24,11 +24,7 @@ public class SitemapController {
 
 
     private final List<String> DOMAINS = List.of(
-            "https://kkidslife.com",
-            "https://www.kkidslife.com",
-            "https://kkidsname.com",
-            "https://www.kkidsname.com"            
-            
+            "https://kkidslife.com"            
     );
 
     @GetMapping(value = "/sitemap.xml", produces = "application/xml; charset=UTF-8")
